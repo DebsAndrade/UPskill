38e3f8a8d25d29c6249edee3dd5090cd211c95a1
doandrade <doandrade@tce.pb.gov.br>
2025-11-06 12:56:57 +0000

    feat: adiciona informações sobre mim.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Passo 1: MOSTRAR a mensagem de boas-vindas
-  console.log("Olá! Sou a Débora Andrade!");
-  console.log("Olá! Sou a Débora Andrade e tenho 30 anos!");
+  console.log("Olá! Sou a Débora Ellen Oliveira de Andrade!");
+  console.log("Tenho " + (20 + 10) + " anos");
+  console.log("Gosta de Programar?");
+  console.log(true);
+  console.log("A aula de hoje foi a " + 3 + " e foi sobre configuração do ambiente e bases do JavaScript.");
 
   // Passo 2: Mostrar a soma (Processamento Simples)
   console.log(10 + 5); 
